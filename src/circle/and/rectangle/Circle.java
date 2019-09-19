4d72c646f1e52dc25709fc6f5967e695f9206eb8
pardoje_cis21035 <pardoje_cis21035@ADMINRG-I180BTA.ceaccs.net>
2019-09-18 23:24:39 -0700

    I commit.

--- src/circle/and/rectangle/Circle.java
+++ src/circle/and/rectangle/Circle.java
@@ -12,12 +12,47 @@ import java.util.Scanner;
  */
 public class Circle {
     
-    public void getRadius(){
+    double radius = 1, area, circumference;
+    
+    public double getRadius(){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the Circle's Radius:");
         double value = input.nextDouble();
         System.out.println("This is the Radius entered: " +value);
+        return value;
     }
     
+    public void setRadius(double x){
+        radius = x;
+    }
     
+    public double getArea(){
+        double res;
+            res = Math.PI * Math.pow(radius, 2);
+        return res;
+    }
+    
+    public double getCircumference(){
+        double circ;
+            circ = 2*Math.PI*radius;
+        return circ;
+    }
+    
+    public void toStreng(double ra, double ci, double ar){
+        radius = ra;
+        circumference = ci;
+        area = ar;
+        System.out.println("Circle Radius = " +radius);
+        System.out.println("Circle Circumference = " +circumference);
+        System.out.println("Circle Area = " +area); 
+    }
 }
+
+/*  
+    Circle
+
+    radius:double = 1.0
+    getRadius(): double 
+    
+    
+*/
